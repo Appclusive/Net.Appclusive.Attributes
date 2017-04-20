52c19bd6b47616b05eade5bd50c85aeecb6716c9
Ronald Rink <ronald.rink@d-fens.net>
2017-04-20 14:14:19 +0200

    Initial Setup
    * added project
    * added approval models

---
+++ src/Net.Appclusive.Attributes/V001/Approval4.cs
@@ -0,0 +1,45 @@
+﻿/**
+ * Copyright 2017 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using Net.Appclusive.Public.Domain.Control;
+using Net.Appclusive.Public.Engine;
+using PublicConstants = Net.Appclusive.Public.Constants;
+
+namespace Net.Appclusive.Attributes.V001
+{
+    public class Approval4 : BaseModel
+    {
+        [AttributeName(PublicConstants.Approval.Approval4.RoleId)]
+        [Range(0, long.MaxValue)]
+        public virtual long RoleId { get; set; }
+
+        [AttributeName(PublicConstants.Approval.Approval4.UserId)]
+        [Range(0, long.MaxValue)]
+        public virtual long UserId { get; set; }
+
+        [AttributeName(PublicConstants.Approval.Approval4.Type)]
+        [Range((long) ApprovalType.Default, (long) ApprovalType.AutoDecline)]
+        public virtual long Type { get; set; }
+
+        [AttributeName(PublicConstants.Approval.Approval4.AbsoluteExpiration)]
+        public virtual long AbsoluteExpiration { get; set; }
+
+        [AttributeName(PublicConstants.Approval.Approval4.RelativeExpiration)]
+        public virtual long RelativeExpiration { get; set; }
+    }
+}
